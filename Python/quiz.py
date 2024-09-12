279c8d77ae77cb9d9e3b7656104f36bfb52c9873
0d3ss3y <samuelkhoali002@gmail.com>
2024-09-12 23:55:17 +0200

    reader structure done

--- Python/quiz.py
+++ Python/quiz.py
@@ -12,6 +12,7 @@ def displayRules():
 def main():
     displayHeader()
     displayRules()
+    
 
 if __name__ == "__main__":
     main()
---
+++ Python/reader/data_reader.py
@@ -0,0 +1,15 @@
+def read():
+    pass
+
+
+def get_options():
+    pass
+
+
+def get_the_questions():
+    pass
+
+
+def process_data():
+    pass
+    
