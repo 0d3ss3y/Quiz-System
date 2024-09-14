e6a25c8f3b83ad2409e686d101bb1d0b529fdc35
0d3ss3y <samuelkhoali002@gmail.com>
2024-09-14 16:04:28 +0200

    Answer sheet 50%

--- Python/answersheet/answersheet.py
+++ Python/answersheet/answersheet.py
@@ -1,4 +1,46 @@
-def question_sheet(category):
-    pass
+from reader import data_reader
+import quiz
 
+def displayCategory(cate):
+    print(f"Category Selection - {cate}")
+
+
+def get_answer(ques_dict):
+    question = ques_dict["question"]
+    options = ques_dict["options"]
+    answer = ques_dict["answer"]
+    
+    print(f"\nQuestion: {question}\n")
+    
+    for i,opt in enumerate(options):
+        print(f"[{chr(65+i)}] {opt}")
+        
+    try:
+        selected_opt = input("\nEnter an answer: ").upper()[0]
+            
+        if ord("A") <=  ord(selected_opt) <= ord("D"):
+            print(f"You selected: {selected_opt}")
+        else:
+            print(f"Please select a number between A and D.")
+    
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        
+    print()
+    return 0
+        
+    
+
+def sheet(category):
+    score = 0
+    displayCategory(category)
+    question_sheets = data_reader.get_questions(category)
+    #quiz.clearing()
+    for sheet in question_sheets:
+        question_dict = dict(sheet)
+        answer = get_answer(question_dict)
+        score += answer
+        
+        
+        
 
