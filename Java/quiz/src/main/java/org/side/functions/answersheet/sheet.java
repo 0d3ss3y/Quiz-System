569307842704b20d06294a008f6fb4066405f408
0d3ss3y <samuelkhoali002@gmail.com>
2024-09-18 22:22:38 +0200

    answersheet up

--- Java/quiz/src/main/java/org/side/functions/answersheet/sheet.java
+++ Java/quiz/src/main/java/org/side/functions/answersheet/sheet.java
@@ -2,26 +2,35 @@ package org.side.functions.answersheet;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import static org.side.functions.reader.data_reader.*;
 
 public class sheet {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static int question_sheet(String category){
+        int score= 0;
         List<Map<String,String>> ques_sheet = get_Question(category);
 
         for (Map<String,String> ques : ques_sheet) {
             String answer = ques.get("answer");
             String options = ques.get("options");
             System.out.println("Question: " + ques.get("question"));
-            displayOptions(options);
+            String attempt = displayOptions(options);
+            boolean compareAns = answer.equals(attempt);
+
+            if (compareAns)
+                score++;
 
         }
-        return 0;
+        return score;
     }
 
-    private static void displayOptions(String options) {
+    private static String displayOptions(String options) {
         char opt = 'A';
+        String answer = "";
+
         options = options.replace("[", "")
                 .replace("]", "")
                 .replace("\"", "")
@@ -31,6 +40,8 @@ public class sheet {
             System.out.println("["+opt+"] " +option.trim());
             opt++;
         }
-        System.out.println();
+        System.out.print("\nAnswer: ");
+        answer = scanner.nextLine();
+        return answer;
     }
 }
