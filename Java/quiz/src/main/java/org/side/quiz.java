fb1fb531162154d4c0d3dd15ae3dfad071ee6422
0d3ss3y <samuelkhoali002@gmail.com>
2024-09-18 22:24:20 +0200

    Working on final score

--- Java/quiz/src/main/java/org/side/quiz.java
+++ Java/quiz/src/main/java/org/side/quiz.java
@@ -5,10 +5,22 @@ import static org.side.functions.reader.data_reader.*;
 import static org.side.headers.TUI.*;
 
 public class quiz {
+    private static final int test_no = 10;
+
     public static void main(String[] args) {
         displayHeader();
         displayRules();
         String category = get_data();
         int score = question_sheet(category);
+        clearing();
+        displayTotal(score);
+    }
+
+    private static void displayTotal(int score) {
+        System.out.println("================================");
+        System.out.println("        Quiz Completed!");
+        System.out.println("================================");
+        System.out.println("Your total score is: " + score + " out of "+ test_no +".");
+        System.out.println("================================");
     }
 }
